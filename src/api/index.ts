79bde5e5baceb9ee84959f05b72517a9bd22253d
jayvanhu <19579031+jayvanhu@users.noreply.github.com>
2021-10-13 19:00:27 -0700

    Update projects page
    * Expand all project's descriptions
    * Add source code link and more tags to Discord UI

--- src/api/index.ts
+++ src/api/index.ts
@@ -14,7 +14,7 @@ export function getProjects(): Promise<Project[]> {
 		id: id(),
 		title: 'Personal Website',
 		img: personalUrl,
-		description: 'This website that you\'re viewing right now.',
+		description: 'This website that you\'re viewing right now. Bulit with accessibility and responsive design in mind. Occasionally updated to apply newly learned frontend skills.',
 		sourceCodeLink: 'https://github.com/jayvanhu/personal-site',
 		tags: ['React'],
 
@@ -32,13 +32,14 @@ export function getProjects(): Promise<Project[]> {
 		id: id(),
 		title: 'Discord UI',
 		img: discordUrl,
-		description: 'A clone of the UI for the Discord voice chat application.',
-		tags: ['WIP', 'React'],
+		description: 'A clone of the UI for the Discord voice chat application. Uses Docker to run as a full-stack app to query mock data form a GraphQL backend.',
+		tags: ['WIP', 'React', 'Nest.js', 'GraphQL', 'Docker'],
+		sourceCodeLink: 'https://github.com/jayvanhu/discord-ui',
 	}, {
 		id: id(),
 		title: 'Golang Blog Scraper',
 		img: golangBlogUrl,
-		description: 'A web scraper written in Golang to scrape Joel\'s blog.',
+		description: 'A web scraper written in Golang to scrape Joel\'s blog. Utilizes goroutines and rate limiting to scrape the site concurrently and safely. Outputs result to an html file.',
 		tags: ['Golang', 'Scripting'],
 		sourceCodeLink: 'https://github.com/jayvanhu/blog-scrape',
 	}]
